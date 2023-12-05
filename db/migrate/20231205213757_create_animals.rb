a05479093d896c4c28a53523bd53e29d9bc73b19
Joseph Kong <josephk1018@gmail.com>
2023-12-05 15:30:02 -0800

    first user story

---
+++ db/migrate/20231205213757_create_animals.rb
@@ -0,0 +1,12 @@
+class CreateAnimals < ActiveRecord::Migration[7.0]
+  def change
+    create_table :animals do |t|
+      t.string :common
+      t.string :name
+      t.string :scientific
+      t.string :binomial
+
+      t.timestamps
+    end
+  end
+end
